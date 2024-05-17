9eafbd3d7a02b8a1fd304a16005f8c2c518638a7
eunsol <rungjinuna@naver.com>
2024-05-17 11:08:15 +0900

    fix:문구수정

--- src/app/components/ChattingApp.jsx
+++ src/app/components/ChattingApp.jsx
@@ -43,7 +43,7 @@ const ChattingApp = () => {
 
   return (
     <div className='text-white'>
-      <h4>Weather App</h4>
+      <h4>Chatting App</h4>
       <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
         {images.map((image, index) => (
           <Image
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -27,19 +27,22 @@ const HeroSection = () => {
             />
           </h1>
           <p className='text-[#adb7be] text-base sm:text-lg mb-6 lg:text-xl'>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit
-            optio quas doloribus ipsum quaerat facere? Ut ullam quis quo
-            temporibus maiores, ratione quae nisi tempore pariatur debitis
-            consequatur eum eos.
+            안녕하세요, 저는 신입 프론트엔드 개발자 김은솔입니다. 사용자 경험을
+            최우선으로 생각하는 인터페이스를 만드는 것에 열정을 가지고 있습니다.
+            HTML, CSS, JavaScript를 비롯한 다양한 프론트엔드 기술 스택을
+            학습하고 프로젝트에 적용해 왔습니다. React 프레임워크를 사용해본
+            경험이 있으며, 사용자 친화적인 웹 애플리케이션을 개발하는 데 자신이
+            있습니다. 항상 배우고 성장하는 자세로 임하며, 팀의 목표를 달성하는
+            데 기여할 수 있는 개발자가 되고 싶습니다. 감사합니다.
           </p>
           <div>
             <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>
               Hire me
             </button>
             <button className='px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3 '>
-              <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
+              {/* <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
                 Download CV
-              </span>
+              </span> */}
             </button>
           </div>
         </motion.div>
