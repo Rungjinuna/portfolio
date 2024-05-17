e77ebbbe7ff249a662ef13dc5ba62770c24666fc
eunsol <rungjinuna@naver.com>
2024-05-17 13:46:06 +0900

    fix:소개문구수정

--- src/app/components/AboutSections.jsx
+++ src/app/components/AboutSections.jsx
@@ -68,10 +68,14 @@ const AboutSections = () => {
         <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
           <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
           <p className='text-base lg:text-lg'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            harum nostrum, velit iusto nihil ut obcaecati pariatur quasi facilis
-            magni soluta incidunt odio veritatis est quas, sunt, temporibus
-            dignissimos ad?
+            안녕하세요! 저는 8년간 프리랜서 요가강사로 활동하며 다양한 사람들과
+            소통하고, 책임감과 적응력을 길러왔습니다. 사람들과의 소통에 능하며,
+            항상 긍정적인 에너지를 전하는 분위기 메이커입니다.이제는 프론트엔드
+            개발자로 새로운 도전을 시작하려고 합니다. 배우려는 자세와 강한
+            책임감으로 업무에 빠르게 적응하며, 팀원들과 협력하여 최고의 성과를
+            만들어내는 것을 목표로 하고 있습니다. 개발자로서의 여정을 통해
+            새로운 기술을 익히고, 창의적인 문제 해결 능력을 발휘하며 성장해
+            나가고 싶습니다. 감사합니다!
           </p>
           <div className='flex flex-row justify-start mt-8'>
             <TabButton
